3b16b1b48ac28852f832bf0aa13311670367d7be
gashki <20312509+gashki@users.noreply.github.com>
2021-05-23 16:23:14 -0700

    Simplified navbar components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Navbar.css";
-import Vertical from "./Vertical";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="navbar">
-        <NavbarSide align={"left"}>
-          <img className="navbar-logo unselectable" src="./images/logo.png" alt="Hupaze logo" />
-        </NavbarSide>
-        <NavbarSide align={"right"}>
-          <NavbarLink title={"Privacy"} href={"./privacy"} />
-          <NavbarLink title={"Support"} href={"mailto:support@hupaze.com"} />
-        </NavbarSide>
-      </div>
-    );
-  }
-}
-
-const NavbarSide = (props) => (
-  <div className="navbar-side" style={{ textAlign: `${props.align}` }}>
-    {props.children}
-    <Vertical />
+const Navbar = () => (
+  <div className="navbar">
+    <img className="navbar-logo unselectable" src="./images/logo.png" alt="Hupaze logo" />
+    <div style={{ flexGrow: 1 }} />
+    <NavItem label={"Privacy"} href={"./privacy"} />
+    <NavItem label={"Support"} href={"mailto:support@hupaze.com"} />
   </div>
 );
 
-const NavbarLink = (props) => (
-  <a className="navbar-link" href={props.href}>{props.title}</a>
+const NavItem = (props) => (
+  <a className="nav-item" href={props.href}>{props.label}</a>
 );
 
 export default Navbar;
