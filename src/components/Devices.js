ef87696b8c84f6c48def80dc2bde8382353b3e20
gashki <20312509+gashki@users.noreply.github.com>
2021-05-23 16:31:03 -0700

    Combined background and content for Devices component

---
+++ src/components/Devices.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Devices.css";
+
+const Devices = () => (
+  <div className="content devices">
+    <img className="unselectable" src="./images/mobile.png" alt="Mobile screenshots" />
+  </div>
+);
+
+export default Devices;
